f3be7b9ba7c7bccf65a5cd48ea18e997a451c5a6
decimatedTomato <greentomatopower@gmail.com>
2023-03-25 16:17:15 +0100

    Doesn't actually require use of float

--- part1 types/tasks.c
+++ part1 types/tasks.c
@@ -6,6 +6,15 @@ int main() {
     int a = 0xDEAD;
     printf("%d %f %e %c %s %x", a, a, a, a, a, a);
 
+    // How to represent that?
+    unsigned int someones_age;
+    char text[20];
+    enum DayOfWeek { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY } day;
+    unsigned int color[3];
+    char tab;
+    _Bool settings[10];
+    double distance_moon_earth;
+
 
     printf("%u\n", sizeof(int));
     printf("%u\n", sizeof(float));
