57a2cc3f832627987cf2c5f8d3f7389b9049217c
decimatedTomato <greentomatopower@gmail.com>
2023-03-25 16:11:27 +0100

    Kept up to date with master branch

--- part1 types/tasks.c
+++ part1 types/tasks.c
@@ -2,6 +2,11 @@
 #include <stdint.h>
 
 int main() {
+    // Data types
+    int a = 0xDEAD;
+    printf("%d %f %e %c %s %x", a, a, a, a, a, a);
+
+
     printf("%u\n", sizeof(int));
     printf("%u\n", sizeof(float));
 
