b98810f30536fae1ff0de09feaf0237143cdfb37
decimatedTomato <greentomatopower@gmail.com>
2023-03-30 19:27:05 +0200

    Added loop and print to task4

--- part01_types/answers04/task4.c
+++ part01_types/answers04/task4.c
@@ -4,8 +4,11 @@
 
 int main() {
     uint_least64_t val = 0;
-    for (size_t i = 0; i < 64; i++) {
-        val = (val * 2) | (rand() % 2);
+    for (size_t i = 0; i < 100; i++) {
+        for (size_t i = 0; i < 64; i++) {
+            val = (val * 2) | (rand() % 2);
+        }
+        printf("%llu\n", val);
     }
     return 0;
 }
