e88ce570561741c4fe01d472e015ea57b90af687
decimatedTomato <greentomatopower@gmail.com>
2023-04-01 16:34:00 +0200

    Trying to fix my code

--- part02_strings/answers03/task3.c
+++ part02_strings/answers03/task3.c
@@ -1,4 +1,5 @@
 #include "goofy.h"
+#include <stdio.h>
 
 /* Create some sample strings in part3.c. Fill out the functions in goofy.c according to their signatures in goofy.h.
 Then invoke those functions in part3.c using the sample strings you created.
@@ -6,25 +7,25 @@ Print the strings before and after invoking the functions.
 In order to use functions across multiple files compile them in one statement. */
 int main() {
     size_t len1 = 7;
-    char *str1 = {13, 1, 20, 78, 55, 30, 0};
-    size_t len2 = 30;
-    char str2[30] = "The factory must GROW";
-    size_t len3;
-    char *str3 = "Tick tock on the clock, but the party don't stop";
-    len3 = sizeof(str3) / sizeof(char);
+    char str1[7] = {13, 1, 20, 78, 55, 30, 0};
+    // size_t len2 = 30;
+    // char str2[30] = "The factory must GROW";
+    // size_t len3;
+    // char *str3 = "Tick tock on the clock, but the party don't stop";
+    // len3 = sizeof(str3) / sizeof(char);
 
     long sum = strsum_n(str1, len1);
     printf("The sum of the ascii values of %s is %ld\n", str1, sum);
-    long sum = strsum_n(str2, len2);
-    printf("The sum of the ascii values of %s is %ld\n", str2, sum);
-    long sum = strsum_n(str3, len3);
-    printf("The sum of the ascii values of %s is %ld\n", str3, sum);
+    // sum = strsum_n(str2, len2);
+    // printf("The sum of the ascii values of %s is %ld\n", str2, sum);
+    // sum = strsum_n(str3, len3);
+    // printf("The sum of the ascii values of %s is %ld\n", str3, sum);
 
-    strfill_n(str3, 10, 'A');
-    printf("After filling a portion of str3 with %c:\n%s\n", 'A', str3);
+    // strfill_n(str3, 10, 'A');
+    // printf("After filling a portion of str3 with %c:\n%s\n", 'A', str3);
 
-    strmishmash_n(str2, len2, str3, len3);
-    printf("%s\n%s\n", str2, str3);
+    // strmishmash_n(str2, len2, str3, len3);
+    // printf("%s\n%s\n", str2, str3);
 
     return 0;
 }
