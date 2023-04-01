968e25c31656f86614ccb39d4dfca3e9ac66f1e8
decimatedTomato <greentomatopower@gmail.com>
2023-04-01 21:26:38 +0200

    Created task4 answers folder

---
+++ part02_strings/answers04/task4.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+/* This function returns whether or not all alphabetic characters of str are in uppercase */
+bool alph_upper(char *str);
+
+/* This function returns whether all characters of str are alphanumeric */
+bool all_alphanumeric(char *str);
+
+/* This function returns whether there are more numeric characters than alphabetic ones */
+bool more_number_less_letter(char *str);
+
+/* This function returns a copy of str which only includes its space and numeric characters */
+char *numeric_substring(char *str);
